c403317190355ef7d96ab90f9df629ba0b37920b
Boardgame <50511847+boardgame1@users.noreply.github.com>
2020-02-03 16:19:28 +0900

    Add files via upload

---
+++ human_vs_ai.py
@@ -0,0 +1,114 @@
+# coding=utf-8
+import argparse, os
+import numpy as np
+import torch
+from lib import game, mcts, model
+
+piece_str = u"초차포마상사졸漢車包馬象士兵"
+def render(pan_str, player_human):
+    pan = game.decode_binary(pan_str)
+    print("   0  1  2  3  4  5  6  7  8")
+    for y in range(10):
+        s = chr(y+ord('0'))+" "
+        for x in range(9):
+            a = pan[y if player_human>0 else 9-y][x if player_human>0 else 8-x]
+            s += piece_str[a // 10 * 7 + a % 10 - 1] + ('-' if x < 8 else ' ') if a > 0 else \
+                (('┌' if x < 1 else '┬' if x < 8 else '┐') if y < 1 else \
+                ('├' if x < 1 else '╋' if x < 8 else '┤') if y < 9 else \
+                ('└' if x < 1 else '┴' if x < 8 else '┘')) + ('─-' if x < 8 else ' ')
+        print(s)
+        if y<9:
+            print("  │  │  │  │＼│／│  │  │  │ " if y<1 or y==7 else "  │  │  │  │／│＼│  │  │  │ "\
+                if y==1 or y>7 else "  │  │  │  │  │  │  │  │  │ ")
+
+def play_game(net1, steps_before_tau_0, mcts_searches, mcts_batch_size, device="cpu"):
+    assert isinstance(net1, model.Net)
+    assert isinstance(steps_before_tau_0, int) and steps_before_tau_0 >= 0
+    assert isinstance(mcts_searches, int) and mcts_searches > 0
+    assert isinstance(mcts_batch_size, int) and mcts_batch_size > 0
+
+    pan = game.encode_lists([list(i) for i in game.INITIAL_STATE], 0)
+    capturelist = []
+    actionhistory = []
+    cur_player = 0
+    step = 0
+    tau = 1 if steps_before_tau_0 > 0 else 0
+    mctsi = mcts.MCTS()
+
+    result = None
+    a0 = ord('0')
+    s=input('플레이하려는 진영을 선택하세요 0) 초, 1)한 ?')
+    player_human = 0 if int(s)<1 else 1
+
+    while result is None:
+        movelist, _ = game.possible_moves(pan, cur_player, step)
+        if (step<2 and cur_player != player_human) or (step>1 and cur_player == player_human):
+            if step < 2:
+                print("마상 차림을 선택하세요 0) 마상마상, 1) 상마상마, 2) 마상상마, 3) 상마마상")
+            else:
+                render(pan, player_human)
+                if step==2 or step==3:
+                    print("")
+                    print("옮기고자 하는 기물의 가로 번호, 세로 번호, 목적지의 가로 번호, 세로 번호 ex) 0010  한수쉼: 0")
+            action = -1
+            while action<0:
+                s=input((str(step-1) if step>1 else '')+' ? ')
+                if step<2:
+                    if len(s)==1 and s[0]>='0' and s[0]<'4': action = int(s) + 10000
+                elif len(s)==1: action = 0
+                elif len(s)==4 and s[0]>='0' and s[0]<='9' and s[1]>='0' and s[1]<='8' and s[2]>='0' and s[2]<='9' and s[3]>='0' and s[3]<='8':
+                    b1=ord(s[0])-a0
+                    if player_human<1: b1=9-b1
+                    b2 = ord(s[1]) - a0
+                    if player_human < 1: b2 = 8 - b2
+                    b3 = ord(s[2]) - a0
+                    if player_human < 1: b3 = 9 - b3
+                    b4 = ord(s[3]) - a0
+                    if player_human < 1: b4 = 8 - b4
+                    action = (b1*9 + b2)*100 + b3*9+b4
+                if action not in movelist: action = -1
+        else:
+            mctsi.search_batch(mcts_searches, mcts_batch_size, pan,
+                            cur_player, net1, step, device=device)
+            probs, _, movep = mctsi.get_policy_value(pan, movelist, cur_player, tau=tau)
+            action = movelist[np.random.choice(len(movelist), p=movep)]
+            if step>1:
+                if action<1: print('한수쉼')
+                else:
+                    b1=action//100//9
+                    if player_human<1: b1=9-b1
+                    b2 = action//100%9
+                    if player_human < 1: b2 = 8 - b2
+                    b3 = action%100//9
+                    if player_human < 1: b3 = 9 - b3
+                    b4 = action%100%9
+                    if player_human < 1: b4 = 8 - b4
+                    print(chr(b1+a0)+chr(b2+a0)+chr(b3+a0)+chr(b4+a0))
+        pan, won = game.move(pan, action, step)
+        actionhistory.append(action)
+        if won>0:
+            render(pan, player_human)
+            print(('초' if won==1 else '한')+' 승')
+            break
+        cur_player = 1-cur_player
+        step += 1
+        if step >= steps_before_tau_0:
+            tau = 0
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--model", help="The model to play")
+    parser.add_argument("--cuda", default=False, action="store_true", help="Enable CUDA")
+    args = parser.parse_args()
+    device = torch.device("cuda" if args.cuda else "cpu")
+
+    modelfile = args.model if args.model else "./best_model.pth"
+    if os.path.isfile(modelfile):
+        net = model.Net(model.OBS_SHAPE, model.policy_size).to(device)
+        checkpoint = torch.load(modelfile, map_location=lambda storage, loc: storage)
+        net.load_state_dict(checkpoint['model'], strict=False)
+        net.eval()
+
+        play_game(net, 20, 60, 80, device)
+    else:
+        print(modelfile+" 파일이 존재하지 않습니다")
---
+++ self-play.py
@@ -0,0 +1,90 @@
+# coding=utf-8
+import os, sys, getpass
+import time
+import argparse, json
+
+from lib import model, mcts, webFunction
+
+import torch
+
+MCTS_SEARCHES = 10
+MCTS_BATCH_SIZE = 20
+STEPS_BEFORE_TAU_0 = 20
+domain = "https://alphajanggi.net"
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cuda", default=False, action="store_true", help="Enable CUDA")
+    args = parser.parse_args()
+    device = torch.device("cuda" if args.cuda else "cpu")
+
+    username = ""
+    while True:
+        createf = False
+        username = input("user ID (to create, enter 0): ")
+        if username == "": continue
+        if username == '0':
+            username = input("user ID to create: ")
+            if username == "": continue
+            password = getpass.getpass("password: ")
+            password2 = getpass.getpass("password to confirm: ")
+            if password == "" or password!=password2: continue
+            createf = True
+        else:
+            password = getpass.getpass("password: ")
+            if password == "": continue
+        js = {"username":username, "password":password, "createf":createf}
+        hr = webFunction.http_request(domain+"/user4", True, json.dumps(js))
+        if hr == None:
+            print("문제가 지속되면 프로젝트 사이트에서 프로그램을 다시 다운로드하세요.")
+            sys.exit()
+        if hr["status"] == "ok": break
+        if hr["status"] == "dup":
+            print("duplicate user ID"); continue
+        elif hr["status"] == "notexist":
+            print("user ID does not exist or password is incorrect"); continue
+
+    best_idx = 0
+    mhash = 1
+    dfile = "./selfmodel.dat"
+    modelfile = "./best_model.pth"
+    if os.path.isfile(dfile):
+        df = open(dfile, "r"); s = df.readline(); df.close()
+        mhash = int(s[:8], 16)
+        best_idx = int(s[8:], 16)
+    s = domain+"/checkmodel"
+    if mhash >=0: s += "?mhash="+str(mhash)
+    hr = webFunction.http_request(s)
+    if hr == None: sys.exit()
+    if hr["status"] == "download":
+        webFunction.download_file(domain+hr["url"], modelfile)
+        df = open(dfile, "w")
+        s = '{:08x}'.format(hr["hash"]) + '{:x}'.format(hr["idx"])
+        best_idx = hr["idx"]
+        df.write(s)
+        df.close()
+
+    net = model.Net(input_shape=model.OBS_SHAPE, actions_n=model.policy_size).to(device)
+    step_idx = 0
+
+    checkpoint = torch.load(modelfile, map_location=lambda storage, loc: storage);
+    if(checkpoint['best_idx'] != best_idx):
+        print("wrong model file")
+        sys.exit()
+    net.load_state_dict(checkpoint['model'], strict=False)
+    net.eval()
+
+    mcts_store = mcts.MCTS()
+
+    while True:
+        t = time.time()
+        _, game_steps = model.play_game(None, mcts_store, None, net, net, steps_before_tau_0=STEPS_BEFORE_TAU_0,
+                mcts_searches=MCTS_SEARCHES, mcts_batch_size=MCTS_BATCH_SIZE, best_idx=best_idx,
+                                   domain=domain, username=username, device=device)
+        game_nodes = len(mcts_store)
+        dt = time.time() - t
+        speed_steps = game_steps / dt
+        speed_nodes = game_nodes / dt
+        print("Step %d, steps %3d, leaves %4d, steps/s %5.2f, leaves/s %6.2f, best_idx %d" % (
+            step_idx, game_steps, game_nodes, speed_steps, speed_nodes, best_idx))
+        step_idx += 1
