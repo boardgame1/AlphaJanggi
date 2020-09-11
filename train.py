eca146492e4417466e6a289f92a7cae553d85008
Boardgame <50511847+boardgame1@users.noreply.github.com>
2020-09-11 15:07:28 +0900

    Add files via upload

--- train.py
+++ train.py
@@ -21,7 +21,7 @@ BATCH_SIZE = 256
 TRAIN_ROUNDS = 20
 MIN_REPLAY_TO_TRAIN = 10000
 
-BEST_NET_WIN_RATIO = 0.55
+BEST_NET_WIN_RATIO = 0.545
 NUM_PROC = 5
 EVALUATION_ROUNDS = 100
 
@@ -29,9 +29,7 @@ def eval(val, lock, net1, net2, device, cpuf):
     if cpuf: net1.to(device); net2.to(device)
     mcts_stores = [mcts.MCTS(), mcts.MCTS()]
     while True:
-        lock.acquire()
-        are = (val[1]+val[2]) % 2
-        lock.release()
+        are = random.randrange(0, 2)
         r, _ = model.play_game(val, mcts_stores, None, net1=net1 if are<1 else net2,
                 net2=net2 if are<1 else net1, steps_before_tau_0=0,
                             mcts_searches=40, mcts_batch_size=40, best_idx=-1, device=device)
@@ -39,8 +37,8 @@ def eval(val, lock, net1, net2, device, cpuf):
         bf = False
         lock.acquire()
         if r!=None:
-            val[1 if (r > 0.5 and are<1) or (r<-0.5 and are>0) else 2] += 1
-            print("%d/%d"%(val[1],val[2]),end=' ', flush=True)
+            val[1 if (r > 0.5 and are<1) or (r<-0.5 and are>=1) else 2] += 1
+            print("%d:%d %d/%d"%(are,r,val[1],val[2]),end=' ', flush=True)
             if (val[1]+val[2]) % 5 <1: print()
         if val[0]<=0: bf=True
         lock.release()
@@ -50,7 +48,6 @@ def eval(val, lock, net1, net2, device, cpuf):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--cuda", default=False, action="store_true", help="Enable CUDA")
-    parser.add_argument("--inc", default=False, action="store_true", help="Increase resBlockNum")
     parser.add_argument("-m", "--model", help="Model to load")
     parser.add_argument("-tm", "--tmodel", help="Temp model")
     args = parser.parse_args()
@@ -61,32 +58,27 @@ if __name__ == "__main__":
 
     step_idx = 0
 
-    if args.tmodel and args.inc: print('invalid argument'); sys.exit()
-
     checkpoint = torch.load(args.model, map_location=lambda storage, loc: storage)
-    model.resBlockNum = checkpoint['resBlockNum']
-    if args.inc: model.resBlockNum +=1
     best_net = model.Net(input_shape=model.OBS_SHAPE, actions_n=actionTable.AllMoveLength).to(device)
     best_net.load_state_dict(checkpoint['model'], strict=False)
     best_idx = checkpoint['best_idx']
-
+    #print(best_net)
     if args.tmodel:
         checkpoint = torch.load(args.tmodel, map_location=lambda storage, loc: storage)
         if best_idx != checkpoint['best_idx']: print('invalid tmodel'); sys.exit()
-        model.resBlockNum = checkpoint['resBlockNum']
         net = model.Net(input_shape=model.OBS_SHAPE, actions_n=actionTable.AllMoveLength).to(device)
-        net.load_state_dict(checkpoint['model'], strict=False)
+        net.load_state_dict(checkpoint['model'])
     else: net = copy.deepcopy(best_net)
     best_net.eval()
-    resNum = model.resBlockNum
     optimizer = optim.SGD(net.parameters(), lr=LEARNING_RATE, momentum=0.9)
     if args.tmodel: optimizer.load_state_dict(checkpoint['opt'])
-    print('best_idx: '+str(best_idx)+'  resBlockNum: '+str(resNum))
+    print('best_idx: '+str(best_idx))
 
     net.train()
     replay_buffer = collections.deque(maxlen=REPLAY_BUFFER)
     f = open("./train.dat", "r")
     ptime = time.time()
+
     while True:
         for lidx in range(PLAY_EPISODES):
             pan = game.encode_lists([list(i) for i in game.INITIAL_STATE], 0)
@@ -96,9 +88,9 @@ if __name__ == "__main__":
             js = json.loads(s)
             result = -js["result"]
             for idx, (action, probs) in enumerate(js["action"]):
-                """movelist = game.possible_moves(pan, idx%2, idx)
-                if action not in movelist:
-                    print("Impossible action selected "+step_idx+" "+lidx)"""
+                movelist = game.possible_moves(pan, idx%2, idx)
+                #if action not in movelist:
+                #    print("Impossible action selected %d %d"%(step_idx, lidx))
                 probs1 = [0.0] * actionTable.AllMoveLength
                 for n in probs:
                     probs1[n[0]] = n[1]
@@ -127,7 +119,9 @@ if __name__ == "__main__":
             probs_v = torch.FloatTensor(batch_probs).to(device)
             values_v = torch.FloatTensor(batch_values).to(device)
             out_logits_v, out_values_v = net(states_v)
-
+            #traced_script_module = torch.jit.trace(net, states_v)
+            #file_name = os.path.join(saves_path, "best_%d.pt" % (best_idx))
+            #traced_script_module.save(file_name); sys.exit()
             loss_value_v = F.mse_loss(out_values_v.squeeze(-1), values_v)
             loss_policy_v = -F.log_softmax(out_logits_v, dim=1) * probs_v
             loss_policy_v = loss_policy_v.sum(dim=1).mean()
@@ -144,7 +138,7 @@ if __name__ == "__main__":
         f = open('./count.txt', 'w'); cn+=step_idx; f.write(str(cn)+'\n'); f.close()
     fns = args.tmodel if args.tmodel else "best_%d%s.pth" % (best_idx, '_a' if args.inc else '')
     file_name = os.path.join('.', fns)
-    torch.save({'model': net.state_dict(), 'best_idx': best_idx, 'resBlockNum': resNum,
+    torch.save({'model': net.state_dict(), 'best_idx': best_idx,
                 'opt': optimizer.state_dict()}, file_name)
 
     print("Net evaluation started")
@@ -173,13 +167,12 @@ if __name__ == "__main__":
         lock.release()
         running = any(p.is_alive() for p in processes)
         if not running:
-            win_ratio = 1 if mar[1] >= EVALUATION_ROUNDS*BEST_NET_WIN_RATIO else 0
             break
         time.sleep(0.5)
     print()
 
-    if win_ratio >= BEST_NET_WIN_RATIO:
+    if mar[1] >= EVALUATION_ROUNDS*BEST_NET_WIN_RATIO:
         print("Net is better than cur best, sync")
         best_idx += 1
         file_name = os.path.join(saves_path, "best_%d.pth" % (best_idx))
-        torch.save({'model': net.state_dict(), 'best_idx': best_idx, 'resBlockNum': resNum}, file_name)
+        torch.save({'model': net.state_dict(), 'best_idx': best_idx}, file_name)
